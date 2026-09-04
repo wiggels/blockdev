f027c1be95aac96682c782ad52b47daf6dea5bea
github-action-benchmark <github@users.noreply.github.com>
2026-09-04 20:56:15 +0000

    add Benchmark (cargo) benchmark result for 17b0ec0897347ad00392d86cdba950e3cb7b4746

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788554086184,
+  "lastUpdate": 1788555375917,
   "repoUrl": "https://github.com/wiggels/blockdev",
   "entries": {
     "Benchmark": [
@@ -95,6 +95,102 @@ window.BENCHMARK_DATA = {
             "name": "filters/system_filter_256",
             "value": 1305,
             "range": "± 6",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "wiggels@gmail.com",
+            "name": "Hunter Wigelsworth",
+            "username": "wiggels"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "17b0ec0897347ad00392d86cdba950e3cb7b4746",
+          "message": "Merge pull request #9 from wiggels/fix-bench-cache\n\nci: wipe target/criterion before benching",
+          "timestamp": "2026-09-04T15:53:13-05:00",
+          "tree_id": "b979a26ac02f1970daba4d0bdf5a82fd35be3f87",
+          "url": "https://github.com/wiggels/blockdev/commit/17b0ec0897347ad00392d86cdba950e3cb7b4746"
+        },
+        "date": 1788555375404,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "get_devices/spawn_only",
+            "value": 2720612,
+            "range": "± 40040",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "get_devices/parse_live_output",
+            "value": 2183,
+            "range": "± 37",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "get_devices/full_request",
+            "value": 2766495,
+            "range": "± 70243",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "parse_lsblk/small_realistic",
+            "value": 16054,
+            "range": "± 99",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "parse_lsblk/large_256_disks_human_size",
+            "value": 189525,
+            "range": "± 2800",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "parse_lsblk/large_256_disks_byte_size",
+            "value": 80023,
+            "range": "± 731",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "parse_lsblk/size_string_heavy_1024",
+            "value": 345552,
+            "range": "± 1308",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "filters/system_filter_small",
+            "value": 143,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "filters/non_system_filter_small",
+            "value": 168,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "filters/find_by_name_hit",
+            "value": 28,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "filters/find_by_name_miss",
+            "value": 5,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "filters/system_filter_256",
+            "value": 1307,
+            "range": "± 22",
             "unit": "ns/iter"
           }
         ]
